50e7f17afb6368793e4a06eb307f7fc8533eb69c
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-12 03:57:59 +0700

    ReportComponent

--- src/components/ReportComponent.js
+++ src/components/ReportComponent.js
@@ -1,12 +1,23 @@
 import { useContext } from 'react'; //รูปย่อในการใช้ useContext
 import DataContext from '../data/DataContext';//รูปย่อในการใช้ useContext
-
+import './ReportComponent.css'
 const ReportComponent=()=>{
     const {income,expense} = useContext(DataContext)// const {income,expense} สลาย array ที่ส่งมาจาก provider จาก app.js
     return (
         <div>
-            <p>รายรับ : {income}</p>
-            <p>รายจ่าย : {expense}</p>
+            <h4>ยอดคงเหลือ (บาท)</h4>
+            <h1>฿{income-expense}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>ยอดรายรับ</h4>
+                    <p className="report plus">฿{income}</p>
+                </div>
+                <div>
+                    <h4>ยอดรายจ่าย</h4>
+                    <p className="report minus">฿{expense}</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
