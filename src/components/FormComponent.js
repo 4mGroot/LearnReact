c0996fe12133aaee7c6023bdeb56c79ce83b3bdf
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-11 18:37:34 +0700

    saveItem

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,18 +1,25 @@
 import './FormComponent.css'
 const FormComponent =()=>{
-    const inputTitle =(evevt)=>{
-        console.log("Hello world")
+    const inputTitle =(event)=>{
+        console.log(event.target.value)
+    }
+    const inputAmount =(event)=>{
+        console.log(event.target.value)
+    }
+    const saveItem =(event)=>{
+        event.preventDefault() //ไม่ให้หน้า web reset
+        console.log("บันทึกข้อมูล")
     }
     return(
         <div>
-        <form>
+        <form onSubmit={saveItem}>
             <div className="form-control">
                 <label>ชื่อรายการ</label>
                 <input type="text" placeholder="ระบุชื่อรายการของคุฌ" onChange={inputTitle}></input>
             </div>
             <div className="form-control">
                 <label>จำนวนเงิน</label>
-                <input type="text" placeholder="ระบุจำนวนเงิน"></input>
+                <input type="text" placeholder="ระบุจำนวนเงิน" onChange={inputAmount}></input>
             </div>
             <div>
                 <button className="btn" type="submit">บันทึกข้อมูล</button>
