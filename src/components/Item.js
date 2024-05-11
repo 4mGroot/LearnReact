057f1e41bb92ec4dfff8d9465dfc508d7e14238f
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-11 17:22:26 +0700

    propsTypes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
 const Item =(props)=>{ 
     const {title,amount}=props
     return (
         <li>{title}<span>{amount}</span></li>
     )
+}
+Item.propTypes={
+    // title ต้องเป็นสตริง amout ต้องวเป็น number isrequid=บังคับให้ป้อน
+    title:PropTypes.string.isRequired,
+    amount:PropTypes.number.isRequired
 }
 export default Item
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,7 @@
 import Item from "./Item"
 import './Transaction.css'
+import { v4 as uuidv4 } from 'uuid'
+
 const Transaction = ()=>{
     const data=[
         {title:"ค่าไฟ",amount:2000},
@@ -7,21 +9,21 @@ const Transaction = ()=>{
         {title:"ค่าขนม",amount:800}
     ]
     const data1=[
-        {title:"ค่าบลาๆ",amount:2000},
+        {title:"ค่าบลาๆ",amount:3000},
         {title:"ค่าจักกิ้งแก้ส",amount:23},
         {title:"ค่ารถ",amount:800}
     ]
     return (
         <ul className="item-list">
-            <Item title="ค่ารักษาพยาบาล" amount="2000"/>
-            <Item title="เงินเดือน" amount="80000"/>
-            <Item title="ค่าเดินทาง" amount="200"/>
+            {/* <Item title="ค่ารักษาพยาบาล" amount="2000" key ={uuidv4()}/>
+            <Item title="เงินเดือน" amount="80000" key ={uuidv4()}/>
+            <Item title="ค่าเดินทาง" amount="200" key ={uuidv4()}/> */}
             {data.map((element)=>{
-                return <Item title={element.title} amount={element.amount}/>
+                return <Item title={element.title} amount={element.amount} key ={uuidv4()}/>
             })}
             {/* หรือจะเขียนแบบนี้เป็นรูปย่อก็ได้ */}
             {data1.map((element)=>{
-                return <Item {...element}/>
+                return <Item {...element} key ={uuidv4()}/>
             })}
         </ul>
     )
